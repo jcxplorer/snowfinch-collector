594073dad9fc316131cee31004c9cac81b0409c4
Joao Carlos <mail@joao-carlos.com>
2011-03-06 00:50:49 +0200

    Adds visit tracking.

--- lib/snowfinch/collector.rb
+++ lib/snowfinch/collector.rb
@@ -3,6 +3,7 @@ require "mongo"
 require "rack"
 require "rack/request"
 require "uri"
+require "time"
 require "tzinfo"
 require "digest/sha1"
 require "radix62"
@@ -18,15 +19,17 @@ module Snowfinch
       params  = Rack::Request.new(env).params
       site_id = BSON::ObjectId(params["token"])
 
-      site = db["sites"].find_one(site_id)
-
-      if site
-        uri = sanitize_uri(params["uri"])
-
-        visitor_name = params["visitor_name"]
-        visitor_id = params["visitorId"]
+      if site = db["sites"].find_one(site_id)
+        uri  = sanitize_uri(params["uri"])
+        uuid = params["uuid"]
 
         time = TZInfo::Timezone.get(site["tz"]).current_period_and_time.first
+
+        previous_visit_spec = {
+          "s" => site_id,
+          "v" => uuid,
+          "h" => { :$gt => (Time.now.to_i - 15 * 60) }
+        }
 
         db["site_counts"].update(
           { "s" => site_id, "y" => time.year },
@@ -50,6 +53,16 @@ module Snowfinch
               "#{time.mon}.#{time.day}.c" => 1,
               "#{time.mon}.#{time.day}.#{time.hour}.c" => 1
             }
+          },
+          { :upsert => true }
+        )
+
+        db["visits"].update(
+          previous_visit_spec,
+          {
+            :$addToSet => { "p" => hash_uri(uri) },
+            :$set => { "h" => Time.now.to_i },
+            :$inc => { "c" => 1 }
           },
           { :upsert => true }
         )
--- spec/acceptance/data_collection_spec.rb
+++ spec/acceptance/data_collection_spec.rb
@@ -14,6 +14,10 @@ feature "Data collection" do
     Snowfinch::Collector.db["page_counts"].find({}, :sort => "_id").to_a
   end
 
+  let :visits do
+    Snowfinch::Collector.db["visits"].find({}, :sort => "_id").to_a
+  end
+
   background do
     @time = Time.utc(2011, 11, 11, 11, 11)
     Timecop.freeze(@time)
@@ -92,6 +96,62 @@ feature "Data collection" do
     page_counts[3]["1"]["c"].should == 1
     page_counts[3]["1"]["1"]["c"].should == 1
     page_counts[3]["1"]["1"]["6"]["c"].should == 1
+  end
+
+  scenario "Multiple visits" do
+    homepage = "http://snowfinch.net/"
+    archive  = "http://snowfinch.net/archive"
+
+    freeze_utc_time(2011, 1, 1, 10, 0)
+    get path(:uuid => "A", :token => token, :uri => homepage)
+    get path(:uuid => "B", :token => token, :uri => homepage)
+
+    freeze_utc_time(2011, 1, 1, 10, 10)
+    get path(:uuid => "A", :token => token, :uri => homepage)
+
+    freeze_utc_time(2011, 1, 1, 10, 15)
+    get path(:uuid => "B", :token => token, :uri => homepage)
+
+    freeze_utc_time(2011, 1, 1, 10, 30)
+    get path(:uuid => "C", :token => token, :uri => homepage)
+
+    freeze_utc_time(2011, 1, 1, 10, 44, 59)
+    get path(:uuid => "C", :token => token, :uri => archive)
+
+    freeze_utc_time(2011, 1, 1, 10, 55)
+    get path(:uuid => "A", :token => token, :uri => homepage)
+
+    freeze_utc_time(2011, 1, 1, 11, 5)
+    get path(:uuid => "A", :token => token, :uri => homepage)
+
+    visits.count.should == 5
+    visits.each { |doc| doc["s"].should == BSON::ObjectId(token) }
+
+    visits[0]["p"].should == [Snowfinch::Collector.hash_uri(homepage)]
+    visits[0]["v"].should == "A"
+    visits[0]["h"].should == Time.utc(2011, 1, 1, 10, 10).to_i
+    visits[0]["c"].should == 2
+
+    visits[1]["p"].should == [Snowfinch::Collector.hash_uri(homepage)]
+    visits[1]["v"].should == "B"
+    visits[1]["h"].should == Time.utc(2011, 1, 1, 10, 0).to_i
+    visits[1]["c"].should == 1
+
+    visits[2]["p"].should == [Snowfinch::Collector.hash_uri(homepage)]
+    visits[2]["v"].should == "B"
+    visits[2]["h"].should == Time.utc(2011, 1, 1, 10, 15).to_i
+    visits[2]["c"].should == 1
+
+    pages = [homepage, archive].map { |uri| Snowfinch::Collector.hash_uri(uri) }
+    visits[3]["p"].should == pages
+    visits[3]["v"].should == "C"
+    visits[3]["h"].should == Time.utc(2011, 1, 1, 10, 44, 59).to_i
+    visits[3]["c"].should == 2
+
+    visits[4]["p"].should == [Snowfinch::Collector.hash_uri(homepage)]
+    visits[4]["v"].should == "A"
+    visits[4]["h"].should == Time.utc(2011, 1, 1, 11, 5).to_i
+    visits[4]["c"].should == 2
   end
 
 end
